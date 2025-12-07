4816cd2952f08e322048eaa0cf6d60e6984105ff
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-12-07 10:09:09 +0200

    Make targets dynamic when creating app

--- cli/src/jvmMain/resources/project/build.gradle.kts
+++ cli/src/jvmMain/resources/project/build.gradle.kts
@@ -5,5 +5,4 @@ plugins {
     alias(libs.plugins.jetbrains.compose.hotreload) apply false
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.jetbrains.compose.compiler) apply false
-    alias(libs.plugins.android.application) apply false
-}
+{{android_plugin}}}
